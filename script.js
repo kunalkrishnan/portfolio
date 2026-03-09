e704143f4c739525f0df2f12ae5936aa433fb9b2
kunal krishnan <61145182+kunalkrishnan@users.noreply.github.com>
2026-03-09 20:32:31 +0530

    Update script.js
    Signed-off-by: kunal krishnan <61145182+kunalkrishnan@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const question = document.getElementById("question")
+const answer = document.getElementById("answer")
 
+question.innerHTML = "What does Z-Order optimization do in Delta Lake?"
+
+function showAnswer(){
+
+answer.innerHTML = "It improves query performance by clustering related data."
+
+}
