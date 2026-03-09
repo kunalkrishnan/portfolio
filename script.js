23c7ade5af81c4db8c9a9a9b8a0f93580f676ab9
kunal krishnan <61145182+kunalkrishnan@users.noreply.github.com>
2026-03-09 21:41:01 +0530

    Update script.js
    Signed-off-by: kunal krishnan <61145182+kunalkrishnan@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const question = document.getElementById("question")
 const answer = document.getElementById("answer")
 
-question.innerHTML = "What does Z-Order optimization do in Delta Lake?"
+const quizData = [
+
+{
+q:"What does Z-Order optimization do in Delta Lake?",
+a:"It clusters related data together to improve query performance."
+},
+
+{
+q:"What is the purpose of the Silver layer in Medallion architecture?",
+a:"Data cleaning, transformation and enrichment."
+},
+
+{
+q:"What tool orchestrates pipelines in Azure?",
+a:"Azure Data Factory."
+}
+
+]
+
+const random = Math.floor(Math.random()*quizData.length)
+
+question.innerHTML = quizData[random].q
 
 function showAnswer(){
 
-answer.innerHTML = "It improves query performance by clustering related data."
+answer.innerHTML = quizData[random].a
 
 }
